70fc39ea5683c226bc5e83ea6d208e5fb77dbfea
sim <simeonbourne@gmail.com>
2016-03-07 22:21:41 -0400

    Modify folder structure

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ var app = express();
 var root = __dirname;
 
 app.use(bodyParser.urlencoded({extended: false }));
-app.use(express.static(path.join(root, 'public')));
-//Serve bower files for jquery and bootstrap etc...
 app.use('/bower_components',express.static(path.join(root, '/bower_components')));
+app.use('/public',express.static(path.join(root, '/public')));
+
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/index.html', function(req, res){
-	res.render('index')
+app.get('/index', function(req, res){
+	// res.render('index');
+	res.sendFile(root + '/public/views/index.html');
+	console.log("Visited the index page!");
+
 });
 
 app.listen(8080, function(){
