ec1f0f04fabcd58696b014a778a565e26020c585
sim <simeonbourne@gmail.com>
2016-03-08 21:45:07 -0400

    Modify url design

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var _ = require('lodash');
 
 var app = express();
 var root = __dirname;
 
 app.use(bodyParser.urlencoded({extended: false }));
 app.use('/bower_components',express.static(path.join(root, '/bower_components')));
+app.use('/node_modules',express.static(path.join(root, '/node_modules')));
 app.use('/public',express.static(path.join(root, '/public')));
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/index', function(req, res){
-	// res.render('index');
-	res.sendFile(root + '/public/views/index.html');
+//Get Index
+app.get('/index', function(req, res){	
+	//Lookup using res.render() and research the difference
+	res.sendFile(root + '/public/views/view.html');
 	console.log("Visited the index page!");
+});
 
+app.get('/patients', function(req, res){
+	console.log('Visited the patient log in page');
+	//res.sendFile(root + '/public/views/view.html');
+	res.status(302).redirect("/index.html#patients");
+});
+
+app.get('/doctors', function(req, res){
+	console.log('Visited the patient log in page');
+	//res.sendFile(root + '/public/views/view.html');
+	res.status(302).redirect("/index.html#doctors");
 });
 
 app.listen(8080, function(){
 	console.log("App listening on port 8080:");
-
 })
 
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,5 +1,34 @@
 $(function() {
+	$('a#patbtn').on("click", function(event){
+		event.preventDefault();
+		//Load Sign in page for patients
+		console.log("You clicked the patient sign in button!");
 
-	
-   
+		var form = "<div class='input-group input-group-lg'>"+
+		  "<span class='input-group-addon' id='sizing-addon1'>Username</span>"+
+		  "<input type='text' class='form-control' placeholder='Username' aria-describedby='sizing-addon1'>"+
+		  "<span class='input-group-addon' id='sizing-addon1'>Password</span>"+
+		  "<input type='password' class='form-control' placeholder='Password' aria-describedby='sizing-addon1'>"+
+			"</div>";
+
+	$('div.main').html(form);
+
+	});
+
+
+	$('a#docbtn').on("click", function(event){
+		event.preventDefault();
+		//Load Sign in page for doctors
+		console.log("You clicked the doctor sign in button!");
+
+		var form = "<div class='input-group input-group-lg'>"+
+		  "<span class='input-group-addon' id='sizing-addon1'>Username</span>"+
+		  "<input type='text' class='form-control' placeholder='Username' aria-describedby='sizing-addon1'>"+
+		  "<span class='input-group-addon' id='sizing-addon1'>Password</span>"+
+		  "<input type='password' class='form-control' placeholder='Password' aria-describedby='sizing-addon1'>"+
+			"</div>";
+
+	$('div.main').html(form);
+
+	});
 });
